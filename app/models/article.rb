e6a6af48add91ec1cfedbb2aae83c3c2ed8a0eeb
Yusaku Toyoda <yusaku.t.oit.a19060@gmail.com>
2023-07-27 19:58:55 +0900

    記事とタグのアソシエーションの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :tag_articles
+  has_many :tag_articles, dependent: :destroy
   has_many :tags, through: :tag_articles
   with_options presence: true do
     validates :title
